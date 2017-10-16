c807d4d47efd2b201dc7493da38220a6682a9834
ADMIN <Chethandc2014@gmail.com>
2017-10-16 08:59:56 +0530

    Changing Request param to Request body as Post request params will come in request body

--- src/main/java/com/irc/controller/ApplicatinController.java
+++ src/main/java/com/irc/controller/ApplicatinController.java
@@ -2,12 +2,14 @@ package com.irc.controller;
 
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.annotation.JsonRawValue;
 import com.irc.dto.BookingDTO;
 import com.irc.service.BookingService;
 
@@ -18,9 +20,10 @@ public class ApplicatinController {
 	@Autowired
 	BookingService bookingService;
 	
-	@RequestMapping(value="/booking",method=RequestMethod.POST,produces="application/json")
-	@ResponseBody
-	public String bookTicket(@RequestParam BookingDTO bookingDTO) {
+	@RequestMapping(value="/booking",method=RequestMethod.POST,consumes="application/json")
+	@JsonRawValue  @ResponseBody
+	public String bookTicket(@RequestBody BookingDTO bookingDTO) {
+		System.out.println(bookingDTO);
 		JSONObject response = bookingService.bookTiket(bookingDTO);
 		return response.toString();
 		
