917750da8b8803a36a3e89d0a59d726f41ed5c87
ADMIN <Chethandc2014@gmail.com>
2017-10-26 10:30:58 +0530

    Adding BookingDao to BookingService

--- src/main/java/com/irc/service/BookingService.java
+++ src/main/java/com/irc/service/BookingService.java
@@ -1,21 +1,27 @@
 package com.irc.service;
 
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.irc.dao.BookingDao;
 import com.irc.dto.BookingDTO;
 
 @Service
 public class BookingService {
 
+	@Autowired
+	BookingDao bookingDao;
+	
 	@Transactional
 	public JSONObject bookTiket(BookingDTO bookingDTO) {
 		JSONObject response = new JSONObject();
 		try {
 			
+			
 			response.put("isBokingSuccess", true);
-			response.put("bookingInfo", "");
+			response.put("bookingInfo", "sdfsdf");
 			
 		} catch (Exception e) {
 			response.put("isBokingSuccess", false);
