f24bf9eb8cc8bca03937f3332f42fcc94e06a124
ADMIN <Chethandc2014@gmail.com>
2017-10-26 10:20:50 +0530

    Adding BookingDao and AdminDao

--- src/main/java/com/irc/dao/AdminDao.java
+++ src/main/java/com/irc/dao/AdminDao.java
@@ -1,5 +1,50 @@
 package com.irc.dao;
 
-public class AdminDao {
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.irc.entity.Coach;
+import com.irc.entity.Station;
+import com.irc.entity.Trains;
+
+@Repository
+@Transactional
+public class AdminDao extends BaseDaoImpl{
+
+	public void addTrain(Trains train) throws Exception {
+	
+		create(train);
+	}
+
+	public void deleteTrain(int trainId) throws Exception {
+		Trains trains = new Trains();
+		trains.setTrainId(trainId);
+		delete(trains);
+	}
+
+	public void addCoachToTrain(Coach coach) throws Exception {
+
+			create(coach);
+	}
+
+	public void removeCoachFromTrain(int coachId) throws Exception {
+
+		Coach coach = new Coach();
+		coach.setCoachId(coachId);
+		delete(coach);
+	}
+
+	public void addStation(Station station) throws Exception {
+
+		create(station);
+		
+	}
+
+	public void deleteStation(int stationId) throws Exception {
+
+		Station stn=new Station();
+		stn.setStationId(stationId);
+		delete(stn);
+	}
 
 }
--- src/main/java/com/irc/dao/BookingDao.java
+++ src/main/java/com/irc/dao/BookingDao.java
@@ -1,5 +1,23 @@
 package com.irc.dao;
 
-public class BookingDao {
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.irc.entity.TrainCoachSeatBooking;
+
+@Repository
+@Transactional
+public class BookingDao extends BaseDaoImpl{
+
+	
+	public Object bookTicket() throws Exception {
+		TrainCoachSeatBooking  book=new TrainCoachSeatBooking();
+	//	book.setPnrNo((short) 123);
+		Object create = create(book);
+		return create;
+		
+	}
+	
+	
+	
 }
